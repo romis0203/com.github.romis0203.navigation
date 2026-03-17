8e3bfcbbf2eace46a015a354da7cdaabc23dc739
Romeo Miranda Moura <romeomiranda72@gmail.com>
2026-03-17 09:50:22 -0300

    arrumando pastas e annotations

--- app/src/main/java/com/git/romis0203/navigation/screen/ui/theme/Android5navigationbetweenscreensTheme.kt
+++
@@ -1,5 +0,0 @@
-package com.git.romis0203.navigation.screen.ui.theme
-
-import androidx.compose.runtime.Composable
-
-annotation class Android5navigationbetweenscreensTheme(val value: @Composable () -> Unit)
--- app/src/main/java/com/git/romis0203/navigation/screen/ui/theme/Theme.kt
+++ app/src/main/java/com/git/romis0203/navigation/screen/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun NavigationscreenTheme(
+fun Android5navigationbetweenscreensTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
