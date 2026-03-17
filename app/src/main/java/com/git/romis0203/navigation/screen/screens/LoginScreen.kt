a142ad86a39599150b8858d7df9b10b77259d57e
Romeo Miranda Moura <romeomiranda72@gmail.com>
2026-03-16 23:32:26 -0300

    arrumando bug

--- app/src/main/java/com/git/romis0203/navigation/screen/screens/LoginScreen.kt
+++ app/src/main/java/com/git/romis0203/navigation/screen/screens/LoginScreen.kt
@@ -1,11 +1,26 @@
 package com.git.romis0203.navigation.screen.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 @Composable
 fun LoginScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFED145B))
+            .background(Color(0xFFFF5722))
             .padding(32.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
